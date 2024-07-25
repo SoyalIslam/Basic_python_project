1cbe89b7437ecd323fb2b6dd321927ed2a835459
S.I.D <120100736+SoyalIslam@users.noreply.github.com>
2024-07-25 18:40:58 +0530

    Add files via upload

--- Atm_System.py
+++ Atm_System.py
@@ -1,50 +1,53 @@
 import time as t
-Present_amount= 2000
-a = str(input("Pls enter your name: "))
-# <<<<<<<<<< Code for current Time >>>>>>>>>>>>>
-samay=t.strftime('%H:%M:%S')
-time=int(t.strftime('%H'))
-time1=int(t.strftime('%H'))
-print("Time:",samay)
-if time<12:
- print("Good Morning ",a,"!")
-elif time>12 and time<18:
-   print("Good Afternoon",a,'!')
-elif time>18 and time<21:
-   print("Good Evening",a,'!')
-elif time>21 and time<24:
-   print("Hello",a,'!')
-# <<<<<<<<<<< Code For Atm Machine >>>>>>>>>>>>>>
-message = """ 
+present_amount = 2000
+name = input("Please enter your name: ")
+
+# Get current time
+current_time = t.strftime('%H:%M:%S')
+hour = int(t.strftime('%H'))
+
+# Print greeting based on time
+if hour < 12:
+    greeting = "Good Morning"
+elif hour < 18:
+    greeting = "Good Afternoon"
+elif hour < 21:
+    greeting = "Good Evening"
+else:
+    greeting = "Hello"
+
+print("Time:", current_time)
+print(f"{greeting}, {name}!")
+
+# ATM Machine Logic
+message = """
 Type 1 >>>> CHECK BALANCE.
 Type 2 >>>> DEPOSIT.
 Type 3 >>>> WITHDRAWL."""
 print(message)
-task = int(input("Pls enter your option : "))
-if task >=1 and task <=3:
+task = int(input("Please enter your option: "))
+
+if task >= 1 and task <= 3:
     print("Welcome to our Virtual bank")
-    if task ==1:
-        print("Sir here is Your Current Balance: INR",Present_amount)
-        if time1>21 and time1<24:
-          print('Good Night',a,"And Take Care !")
-    elif task ==2:
-        print("Enter Your Amount")
-        amount=int(input())
-        if amount <500:
-         print("Sir you Need to deposit More than: 500")
+    if task == 1:
+        print("Sir here is Your Current Balance: INR", present_amount)
+    elif task == 2:
+        amount = int(input("Enter Your Amount: "))
+        if amount < 500:
+            print("Sir you Need to deposit More than: 500")
         else:
-         print("Thank You !! Sir Your Amoumt has been Deposited")
-         print("Sir here is Your Current balanace",Present_amount + amount)
-         if time>21 and time<24:
-           print('Good Night',a,"And Take Care !")
-    elif task ==3:
-        print("Enter Your Amount")
-        withdrawl=int(input())
-        if withdrawl < Present_amount:
-           print("Thank you Sir!","Here is Your current balance", Present_amount - withdrawl)
+            print("Thank You !! Sir Your Amount has been Deposited")
+            present_amount += amount
+            print("Sir here is Your Current balance", present_amount)
+    elif task == 3:
+        withdrawl = int(input("Enter Your Amount: "))
+        if withdrawl < present_amount:
+            print("Thank you Sir!", "Here is Your current balance", present_amount - withdrawl)
+            present_amount -= withdrawl
         else:
-           print("Insufficiant Balance","Transction Fail!")
-           if time>21 and time<24:
-            print('Good Night',a,"And Take Care !")
+            print("Insufficient Balance", "Transaction Fail!")
+
+    if hour > 21:
+        print('Good Night', name, "And Take Care !")
 else:
-    print("Pls enter the no 1 to 3 !")
+    print("Please enter a number between 1 and 3!")
